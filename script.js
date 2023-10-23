b868638611d613a795d45818576344cc04a0e10d
Joe Nadherny <joenadherny@gmail.com>
2023-10-23 16:38:36 -0600

    Another update

--- script.js
+++ script.js
@@ -1,35 +1,26 @@
-document.getElementById('search-form').addEventListener('submit', function (e) {
-    e.preventDefault();
-  
-    const city = document.getElementById('city-input').value;
-    getWeather(city);
-  });
+const apiKey= 'de0a2f05c02c7557e692c2d99c74b84a';
+const apiURL= 'https://api.openweathermap.org/data/2.5/weather?units=imperial&q=';
 
-async function getWeather(city) {
-    const apiKey= 'de0a2f05c02c7557e692c2d99c74b84a'
-    const units = 'imperial';
+const searchBox = document.querySelector('.search input');
+const searchBtn = document.querySelector('.search.button')
 
-    const requestURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+async function checkWeather (city){
+    const response = await fetch(apiURL + city + 'appid=${apiKey}');
+    let data = await response.json();
+    console.log(data);
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°F';
+    document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
+    document.querySelector('.wind').innerHTML = data.wind.speed + 'mp/h';
+    document.querySelector('.date').innerHTML =data.dt_txt;
 
-    try {
-        const response = await fetch(requestURL);
-
-        if (response.ok) {
-            const data = await response.json();
-            console.log(data);
-            // You can display the weather data on your webpage here if needed.
-        } else {
-            console.error(`Failed to fetch weather data. Status code: ${response.status}`);
-        }
-    } catch (error) {
-        console.error('An error occurred while fetching weather data:', error);
     }
-}
-function displayWeather(data) {
-    const resultsContainer = document.getElementById('results-container');
-    resultsContainer.innerHTML = '';
-}
 
-getWeather('Seattle');
+    searchBtn.addEventListener('click', ()=>{
+        checkWeather(searchBox.value);
+    })
+checkWeather();
 
 
+
+  
