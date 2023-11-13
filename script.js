840e52db6ec0eb7f7c75f1bdad435098995033e0
Joe Nadherny <joenadherny@gmail.com>
2023-11-13 10:37:33 -0700

    Restarting js

--- script.js
+++ script.js
@@ -1,49 +1,29 @@
-const apiKey= 'de0a2f05c02c7557e692c2d99c74b84a';
-const apiURL= 'https://api.openweathermap.org/data/2.5/weather?units=imperial&q=';
 
-const searchBox = document.querySelector('.search input');
-const searchBtn = document.querySelector('.search.button');
-const weatherIcon = document.querySelector('.weather-icon')
+const apiKey = 'de0a2f05c02c7557e692c2d99c74b84a';
+const currentWeatherURL = 'https://api.openweathermap.org/data/2.5/weather?units=imperial&q=';
+const forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?units=imperial&q=';
 
-async function checkWeather (city){
-    const response = await fetch(apiURL + city + 'appid=${apiKey}');
+const searchBox = document.getElementById('text-input');
+const resultsContainer = document.getElementById('results-container');
+const weatherIcon = document.querySelector('.weather-icon');
+const searchHistory = document.getElementById('search-history');
 
-    if(response.status == 404){
-        document.querySelector('.error').style.display = 'block';
-        document.querySelector('.weather').style.display = 'none';
+function checkWeather() {
+  const city = searchBox.value;
+  const currentWeatherPromise = fetch(`${currentWeatherURL}${city}&appid=${apiKey}`);
+  const forecastPromise = fetch(`${forecastURL}${city}&appid=${apiKey}`);
 
-    }else{let data = await response.json();
-    
-        document.querySelector(".city").innerHTML = data.name;
-        document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°F';
-        document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
-        document.querySelector('.wind').innerHTML = data.wind.speed + 'mp/h';
-        document.querySelector('.date').innerHTML =data.dt_txt;
-    
-        if(data.weather[0].main == 'Clouds'){
-            weatherIcon.src= 'images/clouds.png';
-    
-        }else if(data.weather[0]. main== 'Clear'){
-            weatherIcon.src ='images/clear.png';
-        }
-        else if(data.weather[0]. main== 'Rain'){
-            weatherIcon.src ='images/rain.png';
-    
-        }else if(data.weather[0]. main== 'Drizzle'){
-            weatherIcon.src ='images/drizzle.png';
-    
-        }else if(data.weather[0]. main== 'Mist'){
-            weatherIcon.src ='images/mist.png';
-        }
-        document.querySelector('.weather').lastElementChild.display = 'block';
-        document.querySelector('.error').style.display = 'none';
-    }
-   
-    searchBtn.addEventListener('click', ()=>{
-        checkWeather(searchBox.value);
+  Promise.all([currentWeatherPromise, forecastPromise])
+    .then((responses) => Promise.all(responses.map((res) => res.json())))
+    .then(([currentWeatherData, forecastData]) => {
+      displayCurrentWeather(currentWeatherData);
+      displayForecast(forecastData);
+      addToSearchHistory(city);
     })
-checkWeather();
-
-
+    .catch((error) => {
+      console.error('Error fetching data:', error);
+      displayError();
+    });
 }
+
   
